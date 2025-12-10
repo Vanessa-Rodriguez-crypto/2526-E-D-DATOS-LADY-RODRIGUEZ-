c853811bd7a4f535226af836e50ca52c186d1eb8
Vanessa-Rodriguez-crypto <lv.rodriguezc@uea.edu.ec>
2025-12-10 17:59:23 -0500

    Create Estudiante.cs

---
+++ SEMANA 3 - arrays y matrices/Estudiante.cs
@@ -0,0 +1,10 @@
+public class Estudiante
+{
+    public int ID;
+    public string Nombres;
+    public string Apellidos;
+    public string Direccion;
+
+    // Arreglo que almacena tres números de teléfono
+    public string[] Telefonos = new string[3];
+}
