2e0bb206a28080c6ce7d865cee8da6a7505d634d
Vanessa-Rodriguez-crypto <lv.rodriguezc@uea.edu.ec>
2026-01-07 16:10:11 -0500

    Create Program.cs

---
+++ SEMANA 5 -/EJERCICIO 5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Estadistica estadistica = new Estadistica();
+        estadistica.LeerNumeros();
+
+        Console.WriteLine($"Media: {estadistica.CalcularMedia()}");
+        Console.WriteLine($"Desviación típica: {estadistica.CalcularDesviacionTipica()}");
+    }
+}
