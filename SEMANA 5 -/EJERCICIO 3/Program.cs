90641116689d0c481c3a3671bc07d7b649a25d07
Vanessa-Rodriguez-crypto <lv.rodriguezc@uea.edu.ec>
2026-01-07 16:13:39 -0500

    Create Program.cs

---
+++ SEMANA 5 -/EJERCICIO 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Loteria loteria = new Loteria();
+        loteria.PedirNumeros();
+        loteria.OrdenarNumeros();
+        loteria.MostrarNumeros();
+    }
+}
