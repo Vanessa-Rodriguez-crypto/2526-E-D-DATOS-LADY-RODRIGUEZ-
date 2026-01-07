082d95e7bddda73cd2db1be8d44d6efe238e0485
Vanessa-Rodriguez-crypto <lv.rodriguezc@uea.edu.ec>
2026-01-07 16:12:00 -0500

    Create Program.cs

---
+++ SEMANA 5 -/EJERCICIO 4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese una palabra: ");
+        string palabra = Console.ReadLine();
+
+        Palindromo p = new Palindromo();
+
+        if (p.EsPalindromo(palabra))
+            Console.WriteLine("La palabra es un palíndromo.");
+        else
+            Console.WriteLine("La palabra NO es un palíndromo.");
+    }
+}
