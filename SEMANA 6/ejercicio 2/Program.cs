9f670065661fa5a3da7cb3b8b7f00b4ced86ce35
Vanessa-Rodriguez-crypto <lv.rodriguezc@uea.edu.ec>
2026-01-12 16:56:31 -0500

    Create Program.cs

---
+++ SEMANA 6/ejercicio 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ListaEnlazada lista = new ListaEnlazada();
+        Random random = new Random();
+
+        // Crear lista con 50 números aleatorios
+        for (int i = 0; i < 50; i++)
+        {
+            lista.Agregar(random.Next(1, 1000));
+        }
+
+        Console.WriteLine("Lista original:");
+        lista.Mostrar();
+
+        // Leer rango desde teclado
+        Console.Write("\nIngrese el valor mínimo del rango: ");
+        int minimo = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el valor máximo del rango: ");
+        int maximo = int.Parse(Console.ReadLine());
+
+        // Eliminar nodos fuera del rango
+        lista.EliminarFueraDeRango(minimo, maximo);
+
+        Console.WriteLine("\nLista después de eliminar nodos fuera del rango:");
+        lista.Mostrar();
+
+        Console.ReadKey();
+    }
+}
